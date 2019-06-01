5d1e010d3276d06f3e7de702cd76093c64b4934c
CoraChang <oll24286@gmail.com>
2019-06-02 00:22:39 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/Lession1Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Lession1Controller : Controller
+    {
+        // GET: Lession1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //protected override void HandleUnknownAction(string actionName)
+        //{
+        //    Response.Redirect("http://www.google.com");
+        //    base.HandleUnknownAction(actionName);
+        //}
+
+
+    }
+}
